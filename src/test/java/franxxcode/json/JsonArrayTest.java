30c49397dc0e17d54b6b3c48dc9458830c376518
Hilmi AM <imiia75775@proton.me>
2024-01-02 13:28:00 +0700

    read json array

--- src/test/java/franxxcode/json/JsonArrayTest.java
+++ src/test/java/franxxcode/json/JsonArrayTest.java
@@ -1,6 +1,7 @@
 package franxxcode.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +20,21 @@ public class JsonArrayTest {
 
         String json = mapper.writeValueAsString(hobbies);
 
+        assertNotNull(json);
+
         System.out.println(json);
+    }
+
+    @Test
+    void read() throws JsonProcessingException {
+        String json = """
+                ["eat","sleep","coding","read manga"]
+                                """;
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<String> hobbies = mapper.readValue(json, new TypeReference<List<String>>() {
+        });
+
+        assertEquals(List.of("eat", "sleep", "coding", "read manga"), hobbies);
     }
 }
