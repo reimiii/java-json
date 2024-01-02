c132a7f70a808766905b3c8f19e91c4f5e2d3b2c
Hilmi AM <imiia75775@proton.me>
2024-01-02 13:17:19 +0700

    read Json Object

--- src/test/java/franxxcode/json/JsonObjectTest.java
+++ src/test/java/franxxcode/json/JsonObjectTest.java
@@ -1,6 +1,7 @@
 package franxxcode.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +29,38 @@ public class JsonObjectTest {
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(person);
 
+        assertNotNull(json);
+
         System.out.println(json);
+    }
+
+    @Test
+    void read() throws JsonProcessingException {
+        String json = """
+                {
+                  "id": "002",
+                  "address": {
+                    "country": "Internet",
+                    "city": "Atlantic",
+                    "street": "Milky way"
+                  },
+                  "username": "franxx",
+                  "age": 20,
+                  "hobbies": [
+                    "Eat Honey",
+                    "Call darling",
+                    "Drawing Darling"
+                  ]
+                }
+                  """;
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String, Object > person = mapper.readValue(json, new TypeReference<Map<String, Object>>() {
+        });
+
+        assertEquals("franxx", person.get("username"));
+        assertEquals(20, person.get("age"));
+        assertEquals(List.of("Eat Honey", "Call darling", "Drawing Darling"), person.get("hobbies"));
     }
 }
