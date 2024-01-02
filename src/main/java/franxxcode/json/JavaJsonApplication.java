2d6b1ab385e794f066002f24143ff4c30115f432
Hilmi AM <imiia75775@proton.me>
2024-01-02 11:39:48 +0700

    Initial commit

---
+++ src/main/java/franxxcode/json/JavaJsonApplication.java
@@ -0,0 +1,13 @@
+package franxxcode.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxxcode/json/JavaJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package franxxcode.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
